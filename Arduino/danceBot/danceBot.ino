0e78920fb19b0f42fb11fbbc4271da180217a6f8
Pierre-Nicolas Tollitte <pierrenicolas.tollitte@gmail.com>
2015-10-08 19:05:43 +0200

    Improved danceBot

--- Arduino/danceBot/danceBot.ino
+++ Arduino/danceBot/danceBot.ino
@@ -2,9 +2,9 @@
 #include <SoftwareSerial.h>
 #include <mBot.h>
 
-#define SPEED                 100    /* Between 0 and 255 */
-#define ROTATION_DURATION     1000   /* Milliseconds */
-#define FORWARD_DURATION      2000   /* Milliseconds */
+#define SPEED                 200    /* Between 0 and 255 */
+#define ROTATION_DURATION     500    /* Milliseconds */
+#define FORWARD_DURATION      1000   /* Milliseconds */
 
 MBot *mBot;
 
@@ -16,27 +16,27 @@ void setup()
 void loop()
 {
     // The mBot robot will rotate on the right, go forward and return
-    // to its original position. Then it will do these 3 steps 5 times.
+    // to its original position. Then it will do these 3 steps 4 times.
     static int index = 0;
-    if (index > 10) {
+    if (index > 3) {
         return;
     }
 
     // Rotate
     mBot->led->setColor(10, 0, 0);
-    mBot->motorL->run(SPEED);
+    mBot->motorL->run(-SPEED);
     mBot->motorR->run(-SPEED);
     delay(ROTATION_DURATION);
 
     // Forward
     mBot->led->setColor(0, 10, 0);
-    mBot->motorL->run(SPEED);
+    mBot->motorL->run(-SPEED);
     mBot->motorR->run(SPEED);
     delay(FORWARD_DURATION);
 
     // Backward
     mBot->led->setColor(0, 0, 10);
-    mBot->motorL->run(-SPEED);
+    mBot->motorL->run(SPEED);
     mBot->motorR->run(-SPEED);
     delay(FORWARD_DURATION);
     
